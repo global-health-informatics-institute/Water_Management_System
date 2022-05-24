84ca5fd23d332433cb263250ebb9d613c6287728
Chatonda <59555994+Chatz102@users.noreply.github.com>
2022-05-24 20:44:48 +0200

    Add files via upload

--- WMS.py
+++ WMS.py
@@ -9,7 +9,6 @@ from ptofaw100_150 import PTOFAW100_150
 import network
 from ntptime import settime
 from machine import Timer
-from microWebSrv import MicroWebSrv
 
 
 # **************************************************
@@ -60,7 +59,7 @@ height2 = 107
 pi = 3.14159265359
 
 #Well Tank thresholds
-maximum_capacity = ((pi*radiusSquare1*(height1/100))*1000)
+maximum_capacity = ((pi*radiusSquare1*(height1/100))*1000) - 10
 minimum_capacity = ((pi*radiusSquare1*(height1/100))*1000)*(15/100)
 mid_capacity = ((pi*radiusSquare1*(height1/100))*1000)*(45/100)
 
@@ -72,15 +71,17 @@ prev1 = 0
 prev2 = 0
 count1 = 0
 count2 = 0
-
+well_counter = 0
+wb_counter = 0
+theSum = 0
 #API URL
-BASE = "https://water-management-system-7d22c-default-rtdb.firebaseio.com/"
+BASE = "https://192.168.0.135"
 
 #Callback function of timer
 def checkWifi():
     count = 0
     #Timer Initialization
-    SSID = "Fadenlauf-2"
+    SSID = "Fadenlauf-1"
     Password = "watchout"
 
     #Connect to Wifi
@@ -97,35 +98,13 @@ def checkWifi():
                 break
             else:
                 count += 1
-                sleep(0.5)
+                sleep(1)
                 pass
     
     if Wifi.isconnected():
         timer.deinit()
         print("Wifi Connected")
         print(Wifi.ifconfig())
-
-pressure = 45
-wb_tank = 860
-well_tank = 4500
-warning1 = 0
-warning2 = 0
-
-#function called when the client sends a GET request on the "/readings" route 
-def _httpHandlerSensorsGet(httpClient, httpResponse):
-    
-    #json object to be sent on client request
-    sensors = {"Pressure":pressure,"Waterboard_Tank":wb_tank,"Well_Tank":well_tank,"Low_Level":warning1,"High_Level":warning2}
-    
-    #sends a json object as a response to the get request from the client
-    httpResponse.WriteResponseJSONOk(
-        obj = sensors,
-        headers = {'Content-Type':'application/json'},)
-routeHandlers = [("/readings","GET", _httpHandlerSensorsGet)]
-srv = MicroWebSrv(routeHandlers=routeHandlers, webPath='/www')
-#starts the server
-srv.Start(threaded=True)
-print("webserver is now running....")
     
 #initialization of network object
 wifi = network.WLAN(network.STA_IF)
@@ -137,30 +116,43 @@ try:
         # GETTING READINGS FROM SENSORS
         # **************************************************   
         
-        #Well Tank Level Sensor
-        initial_height1 = ultra_sensor1.distance_cm()
-        
-        #Filters the sensor data
-        if prev1 == 0:
-            prev1 = initial_height1
-        
-        if initial_height1 != prev1:
-            temp1 = prev1 + 5
-            temp2 = prev1 - 5
-            if initial_height1 > temp1 or initial_height1 < temp2 :
-                print(initial_height1, "cm The filtered distance")
-                initial_height1 = temp1-5
-                count1 += 1
-                if count1 == 10:
-                    prev1 = initial_height1
-                    count1 = 0
-            else:
+        while well_counter < 3:
+            print("entered loop1")
+            #Well Tank Level Sensor
+            initial_height1 = ultra_sensor1.distance_cm()
+            
+            if prev1 == 0:
                 prev1 = initial_height1
-
+            
+            if initial_height1 <= 0:
+                well_counter += 1
+            
+            if initial_height1 != prev1:
+                print("do this")
+                temp1 = prev1 + 5
+                temp2 = prev1 - 5
+                if initial_height1 > temp1 or initial_height1 < temp2 :
+                    print(initial_height1, "cm The filtered distance")
+                    initial_height1 = temp1-5
+                    count1 += 1
+                    if count1 == 10:
+                        prev1 = initial_height1
+                        count1 = 0
+                else:
+                    print("then this")
+                    prev1 = initial_height1
+                    theSum = theSum + initial_height1
+                    well_counter += 1
+                    print("sum is now: ",theSum)
         
+    
+        well_counter = 0
+        wt_height = theSum/3
+        theSum=0
+        print("well tank average height", wt_height)
         
         #well_tank is the value in Litres that is sent to database
-        well_tank = (pi*radiusSquare1*((height1-initial_height1)/100))*1000 # pi*r^2*(b/100) * 1000m^3 to get litres,
+        well_tank = (pi*radiusSquare1*((height1-wt_height)/100))*1000 # pi*r^2*(b/100) * 1000m^3 to get litres,
                                                                             # b = height(cm) - ultrasonic sensed height(cm)
         
         
@@ -170,27 +162,43 @@ try:
         
         
         #Waterboard Tank Level Sensor
-        initial_height2 = ultra_sensor2.distance_cm()
-        
-        #Filters sensor data
-        if prev2 == 0:
-            prev2 = initial_height2
-        
-        if initial_height2 != prev2:
-            temp1 = prev2 + 5
-            temp2 = prev2 - 5
-            if initial_height2 > temp1 or initial_height2 < temp2 :
-                print(initial_height2, "cm The filtered distance")
-                initial_height2 = temp1-5
-                count2 += 1
-                if count2 == 10:
-                    prev2 = initial_height2
-                    count2 = 0
-            else:
+        while wb_counter < 3:
+            print("entered loop2")
+            #Well Tank Level Sensor
+            initial_height2 = ultra_sensor2.distance_cm()
+            
+            if prev2 == 0:
                 prev2 = initial_height2
+            
+            if initial_height2 <= 0:
+                wb_counter += 1
+            
+            if initial_height2 != prev2:
+                print("this is next")
+                temp1 = prev2 + 5
+                temp2 = prev2 - 5
+                if initial_height2 > temp1 or initial_height2 < temp2 :
+                    print(initial_height2, "cm The filtered distance")
+                    initial_height2 = temp1-5
+                    count2 += 1
+                    if count2 == 10:
+                        prev2 = initial_height2
+                        count2 = 0
+                else:
+                    print("this aint happening")
+                    prev1 = initial_height2
+                    theSum = theSum + initial_height2
+                    wb_counter += 1
+                    print("sum is now: ",theSum)
+        
+    
+        wb_counter = 0
+        wbt_height = theSum/3
+        theSum=0
+        print("waterboard tank average height", wbt_height)
         
         #wb_tank is the value in Litres that is sent to database
-        wb_tank = (pi*radiusSquare2*((height2-(initial_height2-23))/100))*1000  #the 23 is Height of water tank - filled depth
+        wb_tank = (pi*radiusSquare2*((height2-wbt_height)/100))*1000  #the 23 is Height of water tank - filled depth
         #ensures the reading of the tank never goes beyond its maximum filled capacity
         if wb_tank > 1111:
             wb_tank = 1111
@@ -211,9 +219,11 @@ try:
         
         #Automatic control mode
         if not overRide:
+            print("entered auto mode")
             
             if (well_tank < minimum_capacity):
                 print("water level too low")
+                warning1 = 1
                 #switch off well valve
                 if(offsetVariable1 == False):
                     valveWell.off()
@@ -224,6 +234,8 @@ try:
                     offsetVariable1 = True
                     offsetVariable2 = True
                     offsetVariable3 = True
+            else:
+                warning1 = 0
                      
             
             if (well_tank > mid_capacity and well_tank < maximum_capacity ):
@@ -239,10 +251,11 @@ try:
                     valveWell.on()
                     offsetVariable1 = False
                     
-                    
-            if (well_tank > maximum_capacity ):
+            #Check if well tank volume is greater than the maximum allowable threshold
+            if (well_tank >= maximum_capacity):
                 sense = valveWell.value()
                 print("water level too high")
+                warning2 = 1
                 #switch on Well-Pump
                 if (offsetVariable3 == True):
                     wellPump.off()
@@ -250,6 +263,8 @@ try:
                 elif(sense == 0):
                     valveWell.on()
                     offsetVariable1 = False
+            else:
+                warning2 = 0
                     
                 
             if(pressure > 62):
@@ -272,6 +287,7 @@ try:
         
         #Manual Control Mode
         elif overRide:
+            print("entered manual mode")
                 
             #toggle well pump on/off
             if value1 and offsetVariable3 == False:
@@ -304,33 +320,7 @@ try:
             elif not value4 and offsetVariable2 == True:
                 valveWB .off()
                 offsetVariable2 = False
-                
-        
-        if wifi.active() and timeSetter == False:
-            settime() #gets the current time from NTP Server only once
-            timeSetter = True
-            
-        
-        if wifi.active():
-            #Creation of Timestamp Key
-            date = rtc.datetime()
-            timestamp = str(date[0])
-            if date[1] < 10:
-                timestamp += ("0"+str(date[1]))
-            else:
-                timestamp += str(date[1])
-            if date[2] < 10:
-                timestamp += ("0"+str(date[2]))
-            else:
-                timestamp += str(date[2])
-            if date[4] < 10:
-                timestamp += ("0"+str(date[4] + 2))
-            else:
-                timestamp += str(date[4] + 2)
-            if date[5] < 10:
-                timestamp += ("0"+str(date[5]))
-            else:
-                timestamp += str(date[5])
+                    
         
         
         #HTTP methods executed only when wifi is available
@@ -341,71 +331,57 @@ try:
             #HTTP PATCH METHOD
             
             #Send Sensor Readings to API 
-            sensors = {timestamp:{"Pressure":pressure,"Waterboard_Tank":wb_tank,"Well_Tank":well_tank}}
+            sensors = {"Pressure":pressure,"WellTank":wellTank,
+               "WbTank":wbTank,"warning1":warning1,"warning2":warning2}
             print('Printing sensor readings')
             print(sensors)
             
             if pressure != 0 and wb_tank > 1 and well_tank > 1 :
                 
-                try:
-                    request_headers = {'Content-Type': 'application/json'}
-                    request = urequests.patch(
-                        BASE+"sensor_data/.json",
-                        json=sensors,
-                        headers=request_headers)
-                    print(request.text)
-                    request.close()
-                    
-                except:
-                    pass
-                
+                request_headers = {'Content-Type': 'application/json'}
+                request = urequests.patch(
+                    BASE+"/sensorValues.php",
+                    json=sensors,
+                    headers=request_headers)
+                print(request.text)
+                request.close()
             else:
                 print("No values for sensors pressure")
             
-            request_headers = {'Content-Type':'application/json'}
+            request_headers = {'Content-Type': 'application/json'}
 
             #If it is operating in Automated mode, send the status of the compoments to database
             if not overRide:
-                components={"wellPump":wellPump.value(),"pressurePump":pressurePump.value(),"wellValve":valveWell.value(),"wbValve":valveWB.value()}
-                
-                try:
-                    request = urequests.patch(
-                        BASE+"commands/.json",
-                        json=components,
-                        headers=request_headers)
-                    print(request.text)
-                    request.close()
-                
-                except:
-                    pass
+                components={"pump1":wellPump.value(),"pump2":pressurePump.value(),"valve1":valveWell.value(),"valve2":valveWB.value()}
+                request = urequests.patch(
+                    BASE+"/sensorValues.php",
+                    json=components,
+                    headers=request_headers)
+                print(request.text)
+                request.close()
                 
             #HTTP GET METHOD
-            try:
-                response = urequests.get(BASE+"commands/.json")
-                
-                if overRide:
-                    if response.status_code == 200:
-                        data = response.json()
-                        overRide = int(data["Mode"])
-                        value1 = int(data["wellPump"])
-                        value2 = int(data["pressurePump"])
-                        value3 = int(data["wellValve"])
-                        value4 = int(data["wbValve"])
-                   
-              
-                else:
-                    if response.status_code == 200:
-                        data = response.json()
-                        overRide = int(data["Mode"])
+            response = urequests.get(BASE+"/sensorValues2.php")
             
-            except:
-                pass
+            if overRide:
+                if response.status_code == 200:
+                    data = response.json()
+                    overRide = int(data["override"])
+                    value1 = int(data["pump1"])
+                    value2 = int(data["pump2"])
+                    value3 = int(data["valve1"])
+                    value4 = int(data["valve2"])
+               
+          
+            else:
+                if response.status_code == 200:
+                    data = response.json()
+                    overRide = int(data["override"])
 
 except:
     print("Error Detected! restarting....")
     machine.reset()
             
-
 
 
 
