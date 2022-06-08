a3ff28ea1b78d0d82711c6d722905f259cb3d7c1
Chatonda <59555994+Chatz102@users.noreply.github.com>
2022-06-08 12:08:24 +0200

    update

--- Server files/session.php
+++ Server files/session.php
@@ -4,7 +4,9 @@ session_start();
 
 //if the user is already logged in then redirect user
 if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true){
-	header("location: index.php");
+	if($_SESSION['name'] == "admin"){
+		header("location: admin.php");
+		}else{header("location: index.php");}
 	exit;
 }
 ?>
