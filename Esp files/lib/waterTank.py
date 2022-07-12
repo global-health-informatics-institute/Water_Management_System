18af687fd80931a7ad1f341f630452f094cf63b5
Chatonda <59555994+Chatz102@users.noreply.github.com>
2022-07-12 11:18:20 +0200

    latest stable updates

--- Esp files/lib/waterTank.py
+++ Esp files/lib/waterTank.py
@@ -18,6 +18,7 @@ class WaterTank:
         self.l = length
         self.w = width
         self.tank_counter = 0
+        self.escape = 0
         self.initial_height = 0
         self.prev1 = 0
         self.tank_type = tank_type
@@ -31,31 +32,42 @@ class WaterTank:
         current_height = 0
         
         while self.tank_counter < 10:
-            #print("entered loop")
             #Well Tank Level Sensor
             self.initial_height = self.ultra_sensor.distance_cm()
-            
-            if self.prev1 == 0:
-                self.prev1 = self.initial_height
-            
-            if self.initial_height <= 0:
-                self.tank_counter += 1
-            
-            if self.initial_height != self.prev1:
-                temp1 = self.prev1 + 2
-                temp2 = self.prev1 - 2
                 
-                if self.initial_height > temp1 or self.initial_height < temp2 :
-                    print(self.initial_height, "cm The filtered distance")
-                    self.initial_height = temp1-5
-                    print("initial height is now: ",self.initial_height)
-                    theSum = theSum + self.initial_height
-                    self.tank_counter += 1
-                    print("sum 1 is now: ",theSum)
-                    count += 1
-                    if count == 30:
+            #discard value that is equal or less than 0
+            if self.initial_height <= 0:
+                self.escape += 1
+                if self.escape == 20:
+                    break
+                pass
+            #only work with positive values
+            elif self.initial_height > 0:
+                if self.prev1 == 0:
+                    self.prev1 = self.initial_height
+                
+                if self.initial_height != self.prev1:
+                    temp1 = self.prev1
+                    temp2 = self.prev1
+                    temp1 = temp1 + 2
+                    temp2 = temp2 - 2
+                    
+                    if self.initial_height > temp1 or self.initial_height < temp2 :
+                        print(self.initial_height, "cm The filtered distance")
+                        self.initial_height = self.prev1
+                        print("initial height is now: ",self.initial_height)
+                        theSum = theSum + self.initial_height
+                        self.tank_counter += 1
+                        print("sum 1 is now: ",theSum)
+                        count += 1
+                        if count == 30:
+                            self.prev1 = self.initial_height
+                            count = 0
+                    else:
                         self.prev1 = self.initial_height
-                        count = 0
+                        theSum = theSum + self.initial_height
+                        self.tank_counter += 1
+                        print("sum is now: ",theSum)
                 else:
                     self.prev1 = self.initial_height
                     theSum = theSum + self.initial_height
@@ -64,11 +76,17 @@ class WaterTank:
         
 
         self.tank_counter = 0
-        current_height = theSum/10
-        print("The current height is now",current_height)
-        theSum=0
-        sleep(1)
-        
+        self.escape = 0
+        #check if there is a valid sum
+        if theSum > 0:
+            current_height = theSum/10
+            print("The current height is now",current_height)
+            theSum=0
+            sleep(1)
+        #assign the previous valid value
+        else:
+            current_height = self.prev1
+            
         #vertical cylindrical tank
         if self.tank_type == "vCylindrical":
             #well_tank is the value in Litres that is sent to database
