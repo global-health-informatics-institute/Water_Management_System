b0624d4371a543f732b6d0518ba4c5dcd58d31fd
Chatonda <59555994+Chatz102@users.noreply.github.com>
2022-08-31 10:23:18 +0200

    [update]

--- Database/WMS.sql
+++ Database/WMS.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.6deb4+deb9u2
+-- version 4.6.6deb5
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Aug 02, 2022 at 03:10 PM
--- Server version: 10.1.48-MariaDB-0+deb9u2
--- PHP Version: 7.0.33-0+deb9u12
+-- Generation Time: Aug 31, 2022 at 10:21 AM
+-- Server version: 10.3.34-MariaDB-0+deb10u1
+-- PHP Version: 7.3.31-1~deb10u1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -46,11 +46,38 @@ CREATE TABLE `commands` (
 --
 
 CREATE TABLE `sensorValues` (
-  `id` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `id` timestamp NOT NULL DEFAULT current_timestamp(),
   `Pressure` varchar(1000) NOT NULL,
   `Volume` varchar(1000) NOT NULL,
   `warning1` int(10) NOT NULL,
   `warning2` int(10) NOT NULL,
+  `watertank_id` int(11) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `settings`
+--
+
+CREATE TABLE `settings` (
+  `well_id` int(11) NOT NULL,
+  `Interval_on` int(11) NOT NULL,
+  `Interval_off` int(11) NOT NULL,
+  `image` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `statistics`
+--
+
+CREATE TABLE `statistics` (
+  `id` int(2) NOT NULL,
+  `Total_consumption` int(10) NOT NULL,
+  `Flow_rate` int(3) NOT NULL,
+  `Record_count` int(11) NOT NULL,
   `watertank_id` int(11) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -99,6 +126,19 @@ ALTER TABLE `sensorValues`
   ADD KEY `watertank_id` (`watertank_id`);
 
 --
+-- Indexes for table `settings`
+--
+ALTER TABLE `settings`
+  ADD PRIMARY KEY (`well_id`);
+
+--
+-- Indexes for table `statistics`
+--
+ALTER TABLE `statistics`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `watertank_id` (`watertank_id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -118,17 +158,27 @@ ALTER TABLE `water_tanks`
 -- AUTO_INCREMENT for table `commands`
 --
 ALTER TABLE `commands`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `settings`
+--
+ALTER TABLE `settings`
+  MODIFY `well_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `statistics`
+--
+ALTER TABLE `statistics`
+  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `water_tanks`
 --
 ALTER TABLE `water_tanks`
-  MODIFY `watertank_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `watertank_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- Constraints for dumped tables
 --
@@ -144,6 +194,12 @@ ALTER TABLE `commands`
 --
 ALTER TABLE `sensorValues`
   ADD CONSTRAINT `sensorValues_ibfk_1` FOREIGN KEY (`watertank_id`) REFERENCES `water_tanks` (`watertank_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `statistics`
+--
+ALTER TABLE `statistics`
+  ADD CONSTRAINT `statistics_ibfk_1` FOREIGN KEY (`watertank_id`) REFERENCES `water_tanks` (`watertank_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
